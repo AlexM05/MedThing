46fd52543b0ea3e816d4f43cf87442034a62b3a7
AlexM05 <47923859+AlexM05@users.noreply.github.com>
2023-03-01 11:54:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import threading
 
 def speak(voice):
     engine = pyttsx3.init()
-    rate = engine.getProperty('rate')
     engine.setProperty('rate', 200)
     engine.say(voice)
     engine.runAndWait()
@@ -18,84 +17,82 @@ def main():
     with sr.Microphone() as source:
         print("Recognizing...")
         text = r.recognize_google(r.record(source, duration=int(3)))
-        print(text)
     if text == "Yes" or text == "yes":
         threading.Thread(target=speak, args=("What kind of pain is it?",)).start()
         with sr.Microphone() as source:
             print("Recognizing...")
             text = r.recognize_google(r.record(source, duration=int(3)))
-            print(text)
-            str_text = repr(text)
-            file.write("Pain Type: \n" + str_text + "\n")
+            file.write("Pain Type: \n" + repr(text) + "\n")
             threading.Thread(target=speak, args=("Is it upper or lower body?",)).start()
-            with sr.Microphone() as source:
+            with sr.Microphone():
                 audio_data = r.record(source, duration=int(3))
                 print("Recognizing...")
                 text = r.recognize_google(audio_data)
-                print(text)
             if text == "upper" or text == "Upper":
                 threading.Thread(target=speak, args=("Is it your head, arms, shoulders, chest, or abdomen?",)).start()
-                with sr.Microphone() as source:
+                with sr.Microphone():
                     print("Recognizing...")
                     text = r.recognize_google(r.record(source, duration=int(3)))
-                    print(text)
                 if text == "head" or text == "Head":
-                    pass
+                    file.write("Location: \n" + repr(text) + "\n")
                 elif text == "arms" or text == "Arms":
+                    file.write("Location: \n" + repr(text))
                     threading.Thread(target=speak, args=("Is it your left or right arm?",)).start()
-                    with sr.Microphone() as source:
+                    with sr.Microphone():
                         print("Recognizing...")
                         text = r.recognize_google(r.record(source, duration=int(3)))
-                        print(text)
                     if text == "left" or text == "Left":
-                        pass
+                        file.write(": " + repr(text))
                     elif text == "right" or text == "Right":
-                        pass
+
+                        file.write(": " + repr(text))
                 elif text == "shoulders" or text == "Shoulders":
+                    file.write("Location: \n" + repr(text))
                     threading.Thread(target=speak, args=("Is it your left or right shoulder?",)).start()
-                    with sr.Microphone() as source:
+                    with sr.Microphone():
                         print("Recognizing...")
                         text = r.recognize_google(r.record(source, duration=int(3)))
-                        print(text)
+                        file.write("Location: \n" + repr(text))
                     if text == "left" or text == "Left":
-                        pass
+                        file.write(": " + repr(text))
                     elif text == "right" or text == "Right":
-                        pass
+                        file.write(": " + repr(text))
                 elif text == "chest" or text == "Chest":
+                    file.write("Location: \n" + repr(text))
                     threading.Thread(target=speak, args=("Is it to the left or right side of your chest?",)).start()
-                    with sr.Microphone() as source:
+                    with sr.Microphone():
                         print("Recognizing...")
                         text = r.recognize_google(r.record(source, duration=int(3)))
-                        print(text)
                     if text == "left" or text == "Left":
-                        pass
+                        file.write(": " + repr(text))
                     elif text == "right" or text == "Right":
-                        pass
+                        file.write(": " + repr(text))
                 elif text == "abdomen" or text == "Abdomen":
                     pass
             elif text == "lower" or text == "Lower":
                 threading.Thread(target=speak, args=("Is it your legs, ankles, feet, groin, or posterior?",)).start()
-                with sr.Microphone() as source:
+                with sr.Microphone():
                     print("Recognizing...")
                     text = r.recognize_google(r.record(source, duration=int(3)))
                     print(text)
                 if text == "legs" or text == "Legs":
-                    pass
+                    file.write("Location: \n" + repr(text))
                 elif text == "ankles" or text == "Ankles":
-                    threading.Thread(target=speak, args=("Is it upper or lower body?",)).start()
-                    speak("Is it your left or right ankle?")
-                    with sr.Microphone() as source:
+                    file.write("Location: \n" + repr(text))
+                    threading.Thread(target=speak, args=("Is it your left or right ankle?",)).start()
+                    with sr.Microphone():
                         print("Recognizing...")
                         text = r.recognize_google(r.record(source, duration=int(3)))
                         print(text)
                     if text == "left" or text == "Left":
-                        pass
+                        file.write(": " + repr(text))
                     elif text == "right" or text == "Right":
-                        pass
+                        file.write(": " + repr(text))
                 elif text == "feet" or text == "Feet":
+                    file.write("Location: \n" + repr(text))
                     threading.Thread(target=speak, args=("Is it upper or lower body?",)).start()
                     speak("Is it your left or right foot?")
-                    with sr.Microphone() as source:
+                    with sr.Microphone():
                         print("Recognizing...")
                         text = r.recognize_google(r.record(source, duration=int(3)))
                         print(text)
@@ -104,9 +101,9 @@ def main():
                     elif text == "right" or text == "Right":
                         pass
                 elif text == "groin" or text == "Groin":
-                    pass
+                    file.write("Location: \n" + repr(text))
                 elif text == "posterior" or text == "Posterior":
-                    pass
+                    file.write("Location: \n" + repr(text))
     elif text == "no" or text == "No":
         threading.Thread(target=speak, args=("What is it that you need?",)).start()
         with sr.Microphone() as source:
